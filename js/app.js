49c8bf83b8ddc37e4904f6b8e898ea783fe4eed6
Beau Doel <beau.doel@gmail.com>
2020-02-16 19:36:30 +0930

    Card deck added and start of functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,42 @@
+//Global Variables
+
+let cardIconArray = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o",
+ "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-cube",
+  "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle",
+   "fa fa-bomb", "fa fa-bomb", "fa fa-bolt", "fa fa-bolt"];
+
+
+const cardDeckContainer = document.querySelector(".deck");
+
+
+//Functionality
+
+//Creating the game board
+
+for(let i = 0; i < cardIconArray.length; i++) {
+    const card = document.createElement("li");
+    const icon = document.createElement("i");
+    card.classList.add("card");
+    icon.className = cardIconArray[i];
+    cardDeckContainer.appendChild(card);
+    card.appendChild(icon);
+
+    card.addEventListener('click', function() {
+        card.classList.add('open', 'show');
+    });
+
+}
 
 
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+
+
+
+
+
+
+
+/* Shuffle function from http://stackoverflow.com/a/2450976
 let shuffleDeck = (array) => {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -14,5 +49,5 @@ let shuffleDeck = (array) => {
     }
 
     return array;
-}
+} */
 
